980988308eed13b108c69b79ec5a606cda29e414
Demonuis <juanandresjaime2406@gmail.com>
2025-09-13 09:43:01 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -10,8 +10,10 @@ $salario = $_POST['salario'];
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <title>Información Recibida</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Información del Empleado</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
 <body>
     <h2>Información recibida</h2>
